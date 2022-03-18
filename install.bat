9bc94c7899037215181bd8229c2980d9df26f943
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-03-18 23:27:27 +0800

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,6 @@
+aria2c --allow-overwrite -d C:\Users\lenovo\.aria2 https://raw.githubusercontent.com/P3TERX/aria2.conf/master/dht.dat
+aria2c --allow-overwrite -d C:\Users\lenovo\.aria2 https://raw.githubusercontent.com/P3TERX/aria2.conf/master/dht6.dat
+aria2c --allow-overwrite -d C:\Users\lenovo\.aria2 https://raw.githubusercontent.com/ZhangShuxiang/ocserv-auto/master/aria2.conf
+aria2c --allow-overwrite -d C:\Users\lenovo\.aria2 https://trackerslist.com/all_aria2.txt
+echo bt-tracker=>>"C:\Users\lenovo\.aria2\aria2.conf"
+type "C:\Users\lenovo\.aria2\all_aria2.txt">>"C:\Users\lenovo\.aria2\aria2.conf"

39cf910a4ffd4ac338707ba986554f13a10688ad
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-06-15 16:33:21 +0800

    Update ocs-auto.sh

--- ocs-auto.sh
+++ ocs-auto.sh
@@ -4,9 +4,6 @@ cd ${basepath}&&mkdir ocsauto&&cd ocsauto
 #########################################
 function ConfigEnvironmentVariable {
     # 变量设置
-    # 服务器的证书和key文件，放在本脚本的同目录下，key文件的权限应该是600或者400
-    servercert=${1-server-cert.pem}
-    serverkey=${2-server-key.pem}
     # 配置目录
     confdir="/etc/ocserv"
     htmldir="/usr/share/nginx/html"
@@ -51,12 +48,10 @@ function InstallOcserv {
 }
 #########################################
 function ConfigOcserv {
-    # 检测是否有证书和 key 文件
-    if [[ ! -f "${servercert}" ]] || [[ ! -f "${serverkey}" ]]; then
-        # 创建 ca 证书和服务器证书（参考http://www.infradead.org/ocserv/manual.html#heading5）
+    # 创建 ca 证书和服务器证书（参考http://www.infradead.org/ocserv/manual.html#heading5）
 #---------------------------------------#
-        certtool --generate-privkey --outfile ca-key.pem
-        cat << _EOF_ >ca.tmpl
+    certtool --generate-privkey --outfile ca-key.pem
+    cat << _EOF_ >ca.tmpl
 cn = "ocservca"
 organization = "ocserv"
 serial = 1
@@ -66,11 +61,11 @@ signing_key
 cert_signing_key
 crl_signing_key
 _EOF_
-        certtool --generate-self-signed --load-privkey ca-key.pem \
-        --template ca.tmpl --outfile ca-cert.pem
+    certtool --generate-self-signed --load-privkey ca-key.pem \
+    --template ca.tmpl --outfile ca-cert.pem
 #---------------------------------------#
-        certtool --generate-privkey --outfile server-key.pem
-        cat << _EOF_ >server.tmpl
+    certtool --generate-privkey --outfile server-key.pem
+    cat << _EOF_ >server.tmpl
 cn = "ocservserver"
 dns_name = "conncet.785118406.xyz"
 organization = "ocserv"
@@ -79,12 +74,12 @@ signing_key
 encryption_key #only if the generated key is an RSA one
 tls_www_server
 _EOF_
-        certtool --generate-certificate --load-privkey server-key.pem \
-        --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem \
-        --template server.tmpl --outfile server-cert.pem
+    certtool --generate-certificate --load-privkey server-key.pem \
+    --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem \
+    --template server.tmpl --outfile server-cert.pem
 #---------------------------------------#
-        certtool --generate-privkey --outfile user-key.pem
-        cat << _EOF_ >user.tmpl
+    certtool --generate-privkey --outfile user-key.pem
+    cat << _EOF_ >user.tmpl
 cn = "ocservuser"
 uid = "${username}"
 unit = "ocserv"
@@ -92,23 +87,21 @@ expiration_days = 3650
 signing_key
 tls_www_client
 _EOF_
-        certtool --generate-certificate --load-privkey user-key.pem \
-        --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem \
-        --template user.tmpl --outfile user-cert.pem
+    certtool --generate-certificate --load-privkey user-key.pem \
+    --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem \
+    --template user.tmpl --outfile user-cert.pem
 #---------------------------------------#
-        certtool --to-p12 --load-privkey user-key.pem \
-        --pkcs-cipher 3des-pkcs12 \
-        --load-certificate user-cert.pem \
-        --outfile user.p12 --outder
+    certtool --to-p12 --load-privkey user-key.pem \
+    --pkcs-cipher 3des-pkcs12 \
+    --load-certificate user-cert.pem \
+    --outfile user.p12 --outder
 #---------------------------------------#
-    fi
-
     # 复制证书
     cp ./server-cert.pem /etc/pki/ocserv/public/server.crt
     cp ./server-key.pem /etc/pki/ocserv/private/server.key
     cp ./ca-cert.pem ${confdir}/ca.pem
     cp ./user.p12 ${htmldir}/user.p12.bak
-
+#---------------------------------------#
     # 编辑配置文件
     (echo "${password}"; sleep 1; echo "${password}") | ocpasswd -c "${confdir}/ocpasswd" ${username}
     cp ${confdir}/ocserv.conf ${confdir}/ocserv.conf.bak
@@ -123,8 +116,9 @@ _EOF_
     sed -i "s@#ipv4-network = 192.168.1.0/24@ipv4-network = 172.16.8.0/24@g" "${confdir}/ocserv.conf"
     sed -i "s@#dns = 192.168.1.2@dns = 8.8.4.4\ndns = 8.8.8.8@g" "${confdir}/ocserv.conf"
     sed -i "s@no-route = 192.168.5.0/255.255.255.0@no-route = 192.168.0.0/16\nno-route = fd00::/64@g" "${confdir}/ocserv.conf"
+#---------------------------------------#
     mv ${htmldir}/index.html ${htmldir}/index.html.bak
-    cat << _EOF_ >index.html
+    cat << _EOF_ >${htmldir}/index.html
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-cn">
   <head>
@@ -135,7 +129,6 @@ _EOF_
   </body>
 </html>
 _EOF_
-    cp ./index.html ${htmldir}/index.html
 }
 #########################################
 function ConfigFirewall {

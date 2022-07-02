8eb104f0649ec8f9ae98963959d2313fdd3d74d4
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-07-02 17:04:57 +0800

    Update ysgm.sh

--- ysgm.sh
+++ ysgm.sh
@@ -22,11 +22,26 @@ gpgkey=https://www.mongodb.org/static/pgp/server-5.0.asc
 _EOF_
 }
 #---------------------------------------------------
-$1
-repo
+install(){
 sudo -E dnf makecache -qy
 sudo -E dnf install -qy epel-release
 sudo -E dnf makecache -qy
 sudo -E dnf install -qy java-17-openjdk.x86_64
 sudo -E dnf install -qy mongodb-org
+}
+#---------------------------------------------------
+#
+#---------------------------------------------------
+firewall(){
+sudo -E firewall-cmd --permanent --add-port=80/tcp
+sudo -E firewall-cmd --permanent --add-port=443/tcp
+sudo -E firewall-cmd --permanent --add-port=8888/tcp
+sudo -E firewall-cmd --permanent --add-port=22102/tcp
+}
+#---------------------------------------------------
+#utsc
+repo
+#
+install
+#firewall
 exit

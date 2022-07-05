912a1b3b3137e1a505fd566e30211d83a63f934d
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-07-05 13:28:38 +0800

    Update ysgm.sh

--- ysgm.sh
+++ ysgm.sh
@@ -34,17 +34,18 @@ firewall(){
 firewall-cmd --permanent --add-port=443/tcp
 firewall-cmd --permanent --add-port=22102/tcp
 }
-#---------------------------------------------------
+#重启一下--------------------------------------------
 Grasscutter(){
 cd $HOME
 git clone https://github.com/Koko-boya/Grasscutter_Resources.git
 git clone https://github.com/Grasscutters/Grasscutter.git
 ln -sf $HOME/Grasscutter_Resources/Resources $HOME/Grasscutter/resources
-cd Grasscutter && chmod +x gradlew && ./gradlew jar
+cd Grasscutter && chmod +x gradlew &&bash ./gradlew jar
 #新建会话tmux new -s ys#退出会话tmux detach#查看会话tmux ls
 #进入会话tmux attach -t ys#结束会话tmux kill-session -t ys#切换会话tmux switch -t ys
 #控制台Ctrl+b 帮助？
 #java -jar grasscutter*.jar -handbook
+#修改一下配置vi config.json
 #java -jar grasscutter*.jar
 }
 #====================================================

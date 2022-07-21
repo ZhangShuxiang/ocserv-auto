acada16c1e3156fef5ab32f48adc6b04b9c9b558
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-07-21 16:42:35 +0800

    Update mailmerge.test.py

--- mailmerge.test.py
+++ mailmerge.test.py
@@ -6,34 +6,34 @@ from mailmerge import MailMerge
 docx = r'E:\Download\py\1\demowd.docx'
 xlsx = r'E:\Download\py\1\demoxl.xlsx'
 
-filedir = os.path.dirname(docx)
-dirname = '证书'+time.strftime("%Y%m%d")
-os.mkdir(dirname)
-filepath = filedir+r'\'+dirname
+time = time.strftime("%Y%m%d%H%M%S")
+filepath = os.path.dirname(xlsx)+'\\证书'+time
+os.mkdir(filepath)
 
-app = xlwings.App(visible=False,add_book=False)
-workbook = app.books.open(xlsx)
+app1 = xlwings.App(visible=False, add_book=False)
+app1.books.open(xlsx)
 worksheet = xlwings.sheets['证书']
 worksheet.range('H:H').api.NumberFormat = "@"
 nrow = worksheet.used_range.last_cell.row
 
-for key in range(1,nrow):
-    with MailMerge(docx) as doc:
-        doc.merge(
-            姓名 = str(worksheet[key, 0].value),
-            性别 = str(worksheet[key, 1].value),
-            出生日期 = str(worksheet[key, 2].value),
-            公司 = str(worksheet[key, 3].value),
-            工作专业 = str(worksheet[key, 4].value),
-            原职务 = str(worksheet[key, 5].value),
-            现职务 = str(worksheet[key, 6].value),
-            证书编号 = str(worksheet[key, 7].value),
-            评审时间 = str(worksheet[key, 8].value),
-            公布时间 = str(worksheet[key, 9].value),
-            公布文号 = str(worksheet[key, 10].value),
-            )
-    
-        output = filepath + r'\证书-{}.docx'.format(str(worksheet[key, 0].value))
-        doc.write(output)
+for key in range(1, nrow):
+    with MailMerge(docx) as document:
+        document.merge(
+            姓名=worksheet[key, 0].value,
+            性别=worksheet[key, 1].value,
+            出生日期=worksheet[key, 2].value,
+            公司=worksheet[key, 3].value,
+            工作专业=worksheet[key, 4].value,
+            原职务=worksheet[key, 5].value,
+            现职务=worksheet[key, 6].value,
+            证书编号=worksheet[key, 7].value,
+            评审时间=worksheet[key, 8].value,
+            公布时间=worksheet[key, 9].value,
+            公布文号=worksheet[key, 10].value)
 
-app.quit()
+        output = filepath + r'\证书-{}.docx'.format(
+            worksheet[key, 0].value+'-' +
+            worksheet[key, 7].value)
+        document.write(output)
+
+app1.quit()

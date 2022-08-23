b8942b869e5e85c8bc973564ac0cbccd7f9dce9f
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2022-08-23 10:28:32 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
-aria2c --allow-overwrite -d C:\Users\lenovo\.aria2 https://raw.githubusercontent.com/P3TERX/aria2.conf/master/dht.dat
-aria2c --allow-overwrite -d C:\Users\lenovo\.aria2 https://raw.githubusercontent.com/P3TERX/aria2.conf/master/dht6.dat
-aria2c --allow-overwrite -d C:\Users\lenovo\.aria2 https://raw.githubusercontent.com/ZhangShuxiang/ocserv-auto/master/aria2.conf
+aria2c --allow-overwrite -d C:\Users\lenovo\.aria2 https://hub.fastgit.xyz/P3TERX/aria2.conf/master/dht.dat
+aria2c --allow-overwrite -d C:\Users\lenovo\.aria2 https://hub.fastgit.xyz/P3TERX/aria2.conf/master/dht6.dat
+aria2c --allow-overwrite -d C:\Users\lenovo\.aria2 https://hub.fastgit.xyz/ZhangShuxiang/ocserv-auto/master/aria2.conf
 aria2c --allow-overwrite -d C:\Users\lenovo\.aria2 https://trackerslist.com/all_aria2.txt
 set /p="bt-tracker="<nul>>aria2.conf 
 type all_aria2.txt>>aria2.conf

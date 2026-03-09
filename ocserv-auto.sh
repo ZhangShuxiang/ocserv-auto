748abaeb17e463a89e5aff6a575980f519acd03e
ZhangShuxiang <66209987+ZhangShuxiang@users.noreply.github.com>
2026-03-09 10:14:45 +0800

    Update ocserv-auto.sh

--- ocserv-auto.sh
+++ ocserv-auto.sh
@@ -1,305 +1,202 @@
 #!/bin/bash
-####################################################
-#                                                  #
-# This is a ocserv installation for CentOS 7       #
-# Version: 0.1.2 2017-02-03
-# Author: Travis Lee                               #
-# Website: https://www.stunnel.info                #
-#                                                  #
-####################################################
-
-# 检测是否是root用户
-if [[ $(id -u) != "0" ]]; then
-    printf "\e[42m\e[31mError: You must be root to run this install script.\e[0m\n"
-    exit 1
-fi
-
-# 检测是否是CentOS 7或者RHEL 7
-# if [[ $(grep "release 7." /etc/redhat-release 2>/dev/null | wc -l) -eq 0 ]]; then
-#     printf "\e[42m\e[31mError: Your OS is NOT CentOS 7 or RHEL 7.\e[0m\n"
-#     printf "\e[42m\e[31mThis install script is ONLY for CentOS 7 and RHEL 7.\e[0m\n"
-#     exit 1
-# fi
-
 basepath=$(dirname $0)
-cd ${basepath}
-
-function ConfigEnvironmentVariable {
-    # 变量设置
-    # 单IP最大连接数，默认是2
-    maxsameclients=10
-    # 最大连接数，默认是16
-    maxclients=1024
-    # 服务器的证书和key文件，放在本脚本的同目录下，key文件的权限应该是600或者400
-    servercert=${1-server-cert.pem}
-    serverkey=${2-server-key.pem}
-    # VPN 内网 IP 段
-    vpnnetwork="192.168.8.0/21"
-    # DNS
-    dns1="8.8.8.8"
-    dns2="8.8.4.4"
-    # 配置目录
+cd ${basepath}&&mkdir ocsauto&&cd ocsauto
+#########################################
+function ConfigEnvironment {
+    #配置目录
     confdir="/etc/ocserv"
-
-    # 获取网卡接口名称
-    # systemctl start NetworkManager.service
-    # ethlist=$(nmcli --nocheck d | grep -v -E "(^(DEVICE|lo)|unavailable|^[^e])" | awk '{print $1}')
-    # eth=$(printf "${ethlist}\n" | head -n 1)
-    eth=eth0
-    # if [[ $(printf "${ethlist}\n" | wc -l) -gt 1 ]]; then
-    #     echo ======================================
-    #     echo "Network Interface list:"
-    #     printf "\e[33m${ethlist}\e[0m\n"
-    #     echo ======================================
-    #     echo "Which network interface you want to listen for ocserv?"
-    #     printf "Default network interface is \e[33m${eth}\e[0m, let it blank to use this network interface: "
-    #     read ethtmp
-    #     if [[ -n "${ethtmp}" ]]; then
-    #         eth=${ethtmp}
-    #     fi
-    # fi
-
-    # 端口，默认是443
-    port=8080
-    # echo -e "\nPlease input the port ocserv listen to."
-    # printf "Default port is \e[33m${port}\e[0m, let it blank to use this port: "
-    # read porttmp
-    # if [[ -n "${porttmp}" ]]; then
-    #     port=${porttmp}
-    # fi
-
-    # 用户名，默认是user
-    username=adminuser
-    # echo -e "\nPlease input ocserv user name."
-    # printf "Default user name is \e[33m${username}\e[0m, let it blank to use this user name: "
-    # read usernametmp
-    # if [[ -n "${usernametmp}" ]]; then
-    #     username=${usernametmp}
-    # fi
-
-    # 随机密码
-    # randstr() {
-    #     index=0
-    #     str=""
-    #     for i in {a..z}; do arr[index]=$i; index=$(expr ${index} + 1); done
-    #     for i in {A..Z}; do arr[index]=$i; index=$(expr ${index} + 1); done
-    #     for i in {0..9}; do arr[index]=$i; index=$(expr ${index} + 1); done
-    #     for i in {1..10}; do str="$str${arr[$RANDOM%$index]}"; done
-    #     echo ${str}
-    # }
-    # password=$(randstr)
-    password=password
-    # printf "\nPlease input \e[33m${username}\e[0m's password.\n"
-    # printf "Random password is \e[33m${password}\e[0m, let it blank to use this password: "
-    # read passwordtmp
-    # if [[ -n "${passwordtmp}" ]]; then
-    #     password=${passwordtmp}
-    # fi
-}
-
-# function PrintEnvironmentVariable {
-#     # 打印配置参数
-#     clear
-
-#     ipv4=$(ip -4 -f inet addr show ${eth} | grep 'inet' | sed 's/.*inet \([0-9\.]\+\).*/\1/')
-#     ipv6=$(ip -6 -f inet6 addr show ${eth} | grep -v -P "(::1\/128|fe80)" | grep -o -P "([a-z\d]+:[a-z\d:]+)")
-#     echo -e "IPv4:\t\t\e[34m$(echo ${ipv4})\e[0m"
-#     if [ ! "$ipv6" = "" ]; then
-#         echo -e "IPv6:\t\t\e[34m$(echo ${ipv6})\e[0m"
-#     fi
-#     echo -e "Port:\t\t\e[34m${port}\e[0m"
-#     echo -e "Username:\t\e[34m${username}\e[0m"
-#     echo -e "Password:\t\e[34m${password}\e[0m"
-#     echo
-#     echo "Press any key to start install ocserv."
-# 
-#     get_char() {
-#         SAVEDSTTY=$(stty -g)
-#         stty -echo
-#         stty cbreak
-#         dd if=/dev/tty bs=1 count=1 2> /dev/null
-#         stty -raw
-#         stty echo
-#         stty ${SAVEDSTTY}
-#     }
-#     char=$(get_char)
-#     clear
-# }
-
-function InstallOcserv {
-    # 升级系统
-    #yum update -y -q
-
-    # 安装 epel-release
-    if [ $(grep epel /etc/yum.repos.d/*.repo | wc -l) -eq 0 ]; then
-        yum install -y -q epel-release && yum clean all && yum makecache fast
+    htmldir="/usr/share/nginx/html"
+    #随机字符串
+    randstr() {
+        index=0
+        str=""
+        for i in {a..z}; do arr[index]=$i; index=$(expr ${index} + 1); done
+        for i in {A..Z}; do arr[index]=$i; index=$(expr ${index} + 1); done
+        for i in {0..9}; do arr[index]=$i; index=$(expr ${index} + 1); done
+        for i in {1..16}; do str="$str${arr[$RANDOM%$index]}"; done
+        echo ${str}
+    }
+    #用户名，默认随机
+    username=$(randstr)
+    echo -e "\nPlease input ocserv user name."
+    printf "Default user name is \e[33m${username}\e[0m, let it blank to use this user name: "
+    read usernametmp
+    if [[ -n "${usernametmp}" ]]; then
+        username=${usernametmp}
     fi
-    # 安装ocserv
-    yum install -y net-tools
-    yum install -y ocserv
+    #密码，默认随机
+    password=$(randstr)
+    printf "\nPlease input \e[33m${username}\e[0m's password.\n"
+    printf "Random password is \e[33m${password}\e[0m, let it blank to use this password: "
+    read passwordtmp
+    if [[ -n "${passwordtmp}" ]]; then
+        password=${passwordtmp}
+    fi
+    #端口
+    echo -n "ocs port: "
+    read porttmp1
+    aadd-port1=${porttmp1}
+    echo -n "ssh port: "
+    read porttmp2
+    aadd-port2=${porttmp2}
+    #域名
+    echo -n "www."
+    read wwwtmp1
+    wwwtmp=${wwwtmp1}
 }
-
-function ConfigOcserv {
-    # 检测是否有证书和 key 文件
-    if [[ ! -f "${servercert}" ]] || [[ ! -f "${serverkey}" ]]; then
-        # 创建 ca 证书和服务器证书（参考http://www.infradead.org/ocserv/manual.html#heading5）
-        certtool --generate-privkey --outfile ca-key.pem
-
-        cat << _EOF_ >ca.tmpl
-cn = "ocserv VPN"
-organization = "ocserv"
+#########################################
+function InstallOcserv {
+    #升级系统
+    dnf update -qqy
+    #安装epel-release
+    dnf install -qqy epel-release
+    #sed -i "0,/enabled=0/s//enabled=1/" /etc/yum.repos.d/epel.repo
+    dnf makecache -qqy
+    #安装ocserv
+    dnf install -qqy ocserv gnutls-utils nginx certbot
+    dnf clean all -qqy
+}
+#########################################
+function InstallCert {
+    #创建证书（参考https://ocserv.openconnect-vpn.net/ocserv.8.html）
+    certtool --generate-privkey --outfile ca-key.pem
+    cat << _EOF_ >ca.tmpl
+cn = "GovernmentCA"
+organization = "Government"
 serial = 1
-expiration_days = 3650
+expiration_days = -1
 ca
 signing_key
 cert_signing_key
 crl_signing_key
 _EOF_
-
-        certtool --generate-self-signed --load-privkey ca-key.pem \
-        --template ca.tmpl --outfile ca-cert.pem
-        certtool --generate-privkey --outfile ${serverkey}
-
-        cat << _EOF_ >server.tmpl
-cn = "ocserv VPN"
-organization = "ocserv"
-serial = 2
-expiration_days = 3650
+    certtool --generate-self-signed --load-privkey ca-key.pem \
+    --template ca.tmpl --outfile ca-cert.pem
+#---------------------------------------#
+    certtool --generate-privkey --outfile server-key.pem
+    cat << _EOF_ >server.tmpl
+cn = "GovServer"
+dns_name = "${wwwtmp}"
+dns_name = "abc.${wwwtmp}"
+organization = "Government"
+expiration_days = -1
 signing_key
-encryption_key #only if the generated key is an RSA one
+encryption_key
 tls_www_server
 _EOF_
-
-        certtool --generate-certificate --load-privkey ${serverkey} \
-        --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem \
-        --template server.tmpl --outfile ${servercert}
-    fi
-
-    # 复制证书
-    cp "${servercert}" /etc/pki/ocserv/public/server.crt
-    cp "${serverkey}" /etc/pki/ocserv/private/server.key
-
-    # 编辑配置文件
-    (echo "${password}"; sleep 1; echo "${password}") | ocpasswd -c "${confdir}/ocpasswd" ${username}
-
-    sed -i 's@auth = "pam"@#auth = "pam"\nauth = "plain[passwd=/etc/ocserv/ocpasswd]"@g' "${confdir}/ocserv.conf"
-    sed -i "s/max-same-clients = 2/max-same-clients = ${maxsameclients}/g" "${confdir}/ocserv.conf"
-    sed -i "s/max-clients = 16/max-clients = ${maxclients}/g" "${confdir}/ocserv.conf"
-    sed -i "s/tcp-port = 443/tcp-port = ${port}/g" "${confdir}/ocserv.conf"
-    sed -i "s/udp-port = 443/udp-port = ${port}/g" "${confdir}/ocserv.conf"
-    sed -i 's/^ca-cert = /#ca-cert = /g' "${confdir}/ocserv.conf"
-    sed -i 's/^cert-user-oid = /#cert-user-oid = /g' "${confdir}/ocserv.conf"
-    sed -i "s/default-domain = example.com/#default-domain = example.com/g" "${confdir}/ocserv.conf"
-    sed -i "s@#ipv4-network = 192.168.1.0/24@ipv4-network = ${vpnnetwork}@g" "${confdir}/ocserv.conf"
-    sed -i "s/#dns = 192.168.1.2/dns = ${dns1}\ndns = ${dns2}/g" "${confdir}/ocserv.conf"
-    sed -i "s/cookie-timeout = 300/cookie-timeout = 86400/g" "${confdir}/ocserv.conf"
-    sed -i 's/user-profile = profile.xml/#user-profile = profile.xml/g' "${confdir}/ocserv.conf"
+    certtool --generate-certificate --load-privkey server-key.pem \
+    --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem \
+    --template server.tmpl --outfile server-cert.pem
+#---------------------------------------#
+    certtool --generate-privkey --outfile user-key.pem
+    cat << _EOF_ >user.tmpl
+dn = "cn=GovUser,O=Government,UID=${username},OU=ocserv"
+expiration_days = -1
+signing_key
+tls_www_client
+_EOF_
+    certtool --generate-certificate --load-privkey user-key.pem \
+    --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem \
+    --template user.tmpl --outfile user-cert.pem
 }
-
+#########################################
+function InstallUserCert {
+    #导出用户证书
+    certtool --to-p12 --load-privkey user-key.pem \
+    --pkcs-cipher 3des-pkcs12 \
+    --load-certificate user-cert.pem \
+    --outfile user.p12 --outder
+}
+#########################################
+function ConfigOcserv {
+    #复制证书文件
+    cp ./server-cert.pem /etc/pki/ocserv/public/server.crt
+    cp ./server-key.pem /etc/pki/ocserv/private/server.key
+    cp ./ca-cert.pem /etc/ocserv/ca.pem
+    cp ./user.p12 ${htmldir}/user.p12.bak
+    #编辑配置文件
+    cp ${confdir}/ocserv.conf ${confdir}/ocserv.conf.bak
+    sed -i 's@^auth\s@#auth\s@g' "${confdir}/ocserv.conf"
+    sed -i 's@#auth = "certificate"@auth = "certificate"@g' "${confdir}/ocserv.conf"
+    sed -i 's@#ca-cert@ca-cert@g' "${confdir}/ocserv.conf"
+    sed -i "s@device = vpns@device = ocservtun@g" "${confdir}/ocserv.conf"
+    sed -i "s@example.com@abc.${wwwtmp}@g" "${confdir}/ocserv.conf"
+    sed -i "s@^no-route\s@#no-route\s@g" "${confdir}/ocserv.conf"
+}
+#########################################
+function ConfigRoute {
+    #添加自定义规则
+    cat << _EOF_ >>${confdir}/ocserv.conf
+ipv4-network = 172.16.8.0/24
+ipv6-network = fd17:2168::/48
+ipv6-subnet-prefix = 128
+tunnel-all-dns = true
+dns = 8.8.8.8
+dns = 2001:4860:4860::8888
+no-route = 192.168.0.0/16
+_EOF_
+}
+#########################################
+function InstallHtml {
+    #添加公益404网页文件
+    mv ${htmldir}/index.html ${htmldir}/index.html.bak
+    cat << _EOF_ >${htmldir}/index.html
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-cn">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  </head>
+  <body>
+        <script src="//cdn.dnpw.org/404/v1.min.js" maincolor="#F00" jumptime="-1" jumptarget="/" tips="404" error="" charset="utf-8"></script>
+  </body>
+</html>
+_EOF_
+}
+#########################################
 function ConfigFirewall {
-
-    firewalldisactive=$(systemctl is-active firewalld.service)
-    iptablesisactive=$(systemctl is-active iptables.service)
-
-    # 添加防火墙允许列表
-    if [[ ${firewalldisactive} = 'active' ]]; then
-        echo "Adding firewall ports."
-        firewall-cmd --permanent --add-port=${port}/tcp
-        firewall-cmd --permanent --add-port=${port}/udp
-        echo "Allow firewall to forward."
-        firewall-cmd --permanent --add-masquerade
-        echo "Reload firewall configure."
-        firewall-cmd --reload
-    elif [[ ${iptablesisactive} = 'active' ]]; then
-        iptables -I INPUT -p tcp --dport ${port} -j ACCEPT
-        iptables -I INPUT -p udp --dport ${port} -j ACCEPT
-        iptables -I FORWARD -s ${vpnnetwork} -j ACCEPT
-        iptables -I FORWARD -d ${vpnnetwork} -j ACCEPT
-        iptables -t nat -A POSTROUTING -s ${vpnnetwork} -o ${eth} -j MASQUERADE
-        #iptables -t nat -A POSTROUTING -j MASQUERADE
-        service iptables save
-    else
-        printf "\e[33mWARNING!!! Either firewalld or iptables is NOT Running! \e[0m\n"
-    fi
+    #编辑系统文件
+    sysctl -w net.ipv4.ip_forward=1 >> /etc/sysctl.conf
+    sysctl -w net.ipv6.conf.all.forwarding=1 >> /etc/sysctl.conf
+    #开启防火墙服务
+    systemctl -q start firewalld.service
+    #添加防火墙允许端口--add-port--remove-port
+    firewall-cmd -q --permanent --add-port=${aadd-port2}/tcp
+    firewall-cmd -q --permanent --add-port=${aadd-port1}/tcp
+    firewall-cmd -q --permanent --add-port=${aadd-port1}/udp
+    firewall-cmd -q --permanent --add-port=80/tcp
+    #开启伪装IP
+    firewall-cmd -q --permanent --add-masquerade
+    firewall-cmd -q --permanent --add-rich-rule='rule family=ipv6 masquerade'
+    #重新加载防火墙
+    firewall-cmd -q --reload
 }
-
-function Installhttpparser {
-    if [[ $(rpm -q http-parser | grep -c "http-parser-2.0") = 0 ]]; then
-        mkdir -p /tmp/http-parser-2.0 /opt/lib
-        cd /tmp/http-parser-2.0
-        wget "http://mirrors.aliyun.com/epel/7/x86_64/h/http-parser-2.0-5.20121128gitcd01361.el7.x86_64.rpm"
-        rpm2cpio http-parser-2.0-5.20121128gitcd01361.el7.x86_64.rpm | cpio -div
-        mv usr/lib64/libhttp_parser.so.2* /opt/lib
-        sed -i 'N;/Type=forking/a\Environment=LD_LIBRARY_PATH=/opt/lib' /lib/systemd/system/ocserv.service
-        sed -i 'N;/Type=forking/a\ExecStartPost=/bin/sleep 0.1' /lib/systemd/system/ocserv.service
-        systemctl daemon-reload
-        cd ~
-        rm -rf /tmp/http-parser-2.0
-    fi
-}
-
+#########################################
 function ConfigSystem {
-    #关闭selinux
-    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
-    setenforce 0
-    #修改系统
-    echo "Enable IP forward."
-    sysctl -w net.ipv4.ip_forward=1
-    echo net.ipv4.ip_forward = 1 >> "/etc/sysctl.conf"
-    systemctl daemon-reload
-    echo "Enable ocserv service to start during bootup."
-    systemctl enable ocserv.service
-    #开启ocserv服务
-    systemctl start ocserv.service
-    echo
+    #添加开机启动
+    systemctl -q enable firewalld.service
+    systemctl -q enable ocserv.service
+    systemctl -q enable nginx.service
+    #开启服务
+    systemctl -q start ocserv.service
+    systemctl -q start nginx.service
 }
-
-# function PrintResult {
-#     #检测防火墙和ocserv服务是否正常
-#     clear
-#     printf "\e[36mChenking Firewall status...\e[0m\n"
-#     iptables -L -n | grep --color=auto -E "(${port}|${vpnnetwork})"
-#     line=$(iptables -L -n | grep -c -E "(${port}|${vpnnetwork})")
-#     if [[ ${line} -ge 2 ]]
-#     then
-#         printf "\e[34mFirewall is Fine! \e[0m\n"
-#     else
-#         printf "\e[33mWARNING!!! Firewall is Something Wrong! \e[0m\n"
-#     fi
-# 
-#     echo
-#     printf "\e[36mChenking ocserv service status...\e[0m\n"
-#     netstat -anptu | grep ":${port}" | grep ocserv-main | grep --color=auto -E "(${port}|ocserv-main|tcp|udp)"
-#     linetcp=$(netstat -anp | grep ":${port}" | grep ocserv | grep tcp | wc -l)
-#     lineudp=$(netstat -anp | grep ":${port}" | grep ocserv | grep udp | wc -l)
-#     if [[ ${linetcp} -ge 1 && ${lineudp} -ge 1 ]]
-#     then
-#         printf "\e[34mocserv service is Fine! \e[0m\n"
-#     else
-#         printf "\e[33mWARNING!!! ocserv service is NOT Running! \e[0m\n"
-#     fi
-# 
-#     #打印VPN参数
-#     printf "
-#     if there are NO WARNING above, then you can connect to
-#     your ocserv VPN Server with the user and password below:
-#     ======================================\n\n"
-#     echo -e "IPv4:\t\t\e[34m$(echo ${ipv4})\e[0m"
-#     if [ ! "$ipv6" = "" ]; then
-#         echo -e "IPv6:\t\t\e[34m$(echo ${ipv6})\e[0m"
-#     fi
-#     echo -e "Port:\t\t\e[34m${port}\e[0m"
-#     echo -e "Username:\t\e[34m${username}\e[0m"
-#     echo -e "Password:\t\e[34m${password}\e[0m"
-# }
-
-ConfigEnvironmentVariable $@
-PrintEnvironmentVariable
+#########################################
+ConfigEnvironment
+echo "ConfigEnvironment Successful!"
 InstallOcserv
+echo "InstallOcserv Successful!"
+InstallCert
+echo "InstallCert Successful!"
+InstallUserCert
+echo "InstallUserCert Successful!"
 ConfigOcserv
+echo "ConfigOcserv Successful!"
+ConfigRoute
+#echo "ConfigRoute Successful!"
+InstallHtml
+echo "InstallHtml Successful!"
 ConfigFirewall
-#Install-http-parser
+echo "ConfigFirewall Successful!"
 ConfigSystem
-PrintResult
-
-exit 0
+echo "ConfigSystem Successful!"
+exit
